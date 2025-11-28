66787b7aa274156c78ea71892568e8de02fa03e9
Gamedosa <gabriel20melo@gmail.com>
2025-11-27 22:36:48 -0300

    feat: funções recebendo objetos.

--- javascript/classe/aula.js
+++ javascript/classe/aula.js
@@ -24,7 +24,7 @@ class Fruta {
 
     constructor(nome,quantidade){
         //Me obriga que quando uma nova fruta for instanciada eu seja obrigado a passar os parametros.
-        
+
         this.nome = nome
         this.quantidade = quantidade
     }
@@ -38,5 +38,22 @@ class Fruta {
 }
 
 fruta1 = new Fruta('banana', 4);
+fruta2 = new Fruta('Maça', 7);
 
-fruta1.descarcar();
+// fruta1.descarcar();
+
+function compararQtdFrutas(fruta1,fruta2){
+    if(fruta1.quantidade > fruta2.quantidade){
+        console.log(`Existem mais ${fruta1.nome} do que ${fruta2.nome}`)
+    }  else if(fruta2.quantidade > fruta1.quantidade){
+        console.log(`Existem mais ${fruta2.nome} do que ${fruta1.nome}`)
+    }
+    else{
+        console.log(`As quantidades de ${fruta1.nome} e ${fruta2.nome} são iguais`);
+    }
+}
+
+const f1 = new Fruta('Abacaxi', 7);
+const f2 = new Fruta('Goiaba', 7)
+
+compararQtdFrutas(f1,f2);
