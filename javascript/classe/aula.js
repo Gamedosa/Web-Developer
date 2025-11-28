b43f191fbdf99f4e383f114a45cff7edfbe6ba3c
Gamedosa <gabriel20melo@gmail.com>
2025-11-27 22:26:04 -0300

    feat: Utilização do metodo Constructor.

--- javascript/classe/aula.js
+++ javascript/classe/aula.js
@@ -16,3 +16,27 @@ pessoa1.nome = 'Gabriel'
 pessoa1.idade = 24
 
 pessoa1.apresentar();
+
+class Fruta {
+    nome;
+    quantidade;
+
+
+    constructor(nome,quantidade){
+        //Me obriga que quando uma nova fruta for instanciada eu seja obrigado a passar os parametros.
+        
+        this.nome = nome
+        this.quantidade = quantidade
+    }
+    
+    /**
+     * metodo para descascar fruta
+     */
+    descarcar() {
+        console.log(`Descascando ${this.quantidade} ${this.nome}`)   
+    }
+}
+
+fruta1 = new Fruta('banana', 4);
+
+fruta1.descarcar();
